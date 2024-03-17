e358a44d0e82f500834e17e8aefd98e9b35e2869
Nyasizza <125912086+Nyasizza@users.noreply.github.com>
2024-03-17 20:42:20 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.nav');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('open');
+});
